7d79ddc82ce581fe34e181ed65a129df45e49379
venomissionary <stevehernandes53@gmail.com>
2024-10-13 15:29:34 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,12 @@ header.innerHTML = gameName;
 app.append(header);
 
 const button = document.createElement("button");
-button.innerText = "Click me 😩 "
-button.style.padding = "10px"
-button.style.fontSize = "16px"
+button.innerText = "Click me 😩 ";
+button.style.padding = "10px";
+button.style.fontSize = "16px";
 
 button.addEventListener("click", () => {
-    console.log("Button has been clicked");
-    
+  console.log("Button has been clicked");
 });
 
 app.append(button);
