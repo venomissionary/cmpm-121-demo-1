6a96040ee462495a929d9a1984a048f4eba25d42
venomissionary <stevehernandes53@gmail.com>
2024-10-14 04:49:09 -0700

    - added a counter. - adjusted to center of window step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,22 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Steven's Game";
 document.title = gameName;
 
+let counter: number = 0;
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
+const button = document.getElementById("Button1") as HTMLButtonElement;
+const counterDisplay = document.getElementById("numberCounter") as HTMLDivElement;
+
 button.innerText = "Click me 😩 ";
 button.style.padding = "10px";
 button.style.fontSize = "16px";
 
 button.addEventListener("click", () => {
-  console.log("Button has been clicked");
+  counter += 1;
+  counterDisplay.textContent = `${counter} cookies `;
 });
 
 app.append(button);
