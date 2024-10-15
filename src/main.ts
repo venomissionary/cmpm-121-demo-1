6e3770fc63e3ef259b994940c637690509060eeb
venomissionary <stevehernandes53@gmail.com>
2024-10-14 19:13:56 -0700

    - Step 4 completed

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,14 @@ const gameName = "Steven's Game";
 document.title = gameName;
 
 let counter: number = 0;
+let time: number = 0;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 const button = document.getElementById("Button1") as HTMLButtonElement;
-const counterDisplay = document.getElementById(
-  "numberCounter",
-) as HTMLDivElement;
+const counterDisplay = document.getElementById("numberCounter",) as HTMLDivElement;
 
 button.innerText = "Click me 😩 ";
 button.style.padding = "10px";
@@ -26,8 +25,22 @@ button.addEventListener("click", () => {
 
 function raiseCounter() {
   counter += 1;
-  counterDisplay.textContent = `${counter} cookies `;
+  counterDisplay.textContent = `${counter.toFixed(2)} cookies `;
 }
+
+function counterUpdate(timeLapse: number) {
+    if (time === 0) {
+        time = timeLapse
+    }
+
+    const delta = timeLapse - time;
+    counter += delta / 1000;
+    counterDisplay.textContent =  `${counter.toFixed(2)} cookies `;
+    time = timeLapse;
+    requestAnimationFrame(counterUpdate);
+}
+
+requestAnimationFrame(counterUpdate);
 
 setInterval(() => {
   raiseCounter();
