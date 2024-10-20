ffac2174dcb49962eefd9b2dc5694b7fd2f03afb
venomissionary <stevehernandes53@gmail.com>
2024-10-19 20:58:24 -0700

    step 8 complete

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 import cookiePicLocal from "./assets/cookie.png";
+import kitchenPicLocal from "./assets/vintageKitchen.png";
+import fryingPanLocal from "./assets/fryingPan.png";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
@@ -36,8 +38,12 @@ button.style.fontSize = "16px";
 const counterDisplay = document.createElement("counter") as HTMLDivElement;
 counterDisplay.id = "numberCounter";
 counterDisplay.textContent = "0 cookies";
-counterDisplay.style.marginTop = "100px";
-counterDisplay.style.fontSize = "20px";
+counterDisplay.style.position = "absolute";
+counterDisplay.style.top = "40%"; 
+counterDisplay.style.left = "42.5%"; 
+counterDisplay.style.transform = "translate(-50%, -50%)"; 
+counterDisplay.style.zIndex = "2"; 
+counterDisplay.style.color = "white"; 
 
 //cookie image
 const cookiePic = document.createElement("img") as HTMLImageElement;
@@ -49,19 +55,50 @@ cookiePic.style.display = "block";
 cookiePic.style.marginLeft = "auto";
 cookiePic.style.marginRight = "auto";
 
+//background setting image
+const kitchenPic = document.createElement("img") as HTMLImageElement;
+kitchenPic.src = kitchenPicLocal;
+kitchenPic.alt = "Kitchen";
+kitchenPic.style.position = "absolute";
+kitchenPic.style.top = "0";
+kitchenPic.style.left = "0";
+kitchenPic.style.width = "100%";
+kitchenPic.style.height = "100%";
+kitchenPic.style.zIndex = "-1";
+kitchenPic.style.objectFit = "cover";
+kitchenPic.style.opacity = "0.2";
+
 //counter for speed rate
 const speedRateDisplay = document.createElement("div");
 speedRateDisplay.textContent = "Speed Rate: 0.00 cookies per second";
 
+//frying pan image
+const fryingPanPic = document.createElement("img") as HTMLImageElement;
+fryingPanPic.src = fryingPanLocal;
+fryingPanPic.alt = "frying pan";
+fryingPanPic.style.position = "relative";
+fryingPanPic.style.width = "50%";
+fryingPanPic.style.height = "auto";
+
+const fryingPanResizeable = document.createElement("div");
+fryingPanResizeable.style.position = "absolute";
+fryingPanResizeable.style.top = "15%"; 
+fryingPanResizeable.style.left = "52%"; 
+fryingPanResizeable.style.width = "600px"; 
+fryingPanResizeable.style.height = "auto";
+fryingPanResizeable.style.transform = "translate(-50%, -50%)"; 
+fryingPanResizeable.style.zIndex = "1";
+
 //purchase history counter
 const buyDisplay = document.createElement("div");
-buyDisplay.textContent = "Purchased - A: 0, B: 0, C: 0";
+buyDisplay.textContent =
+  "Purchased - Fast Bake: 0, Faster Bake: 0, Fastest Bake: 0";
 buyDisplay.style.padding = "20px";
 
 //options for speed upgrades
-const optionA = upgradeButton("Option A (cost 10) ", orginalCostA ,  0.1);
-const optionB = upgradeButton("Option B (cost 100) ", orginalCostB, 2.0);
-const optionC = upgradeButton("Option C (cost 1000) " , orginalCostC, 50.0);
+const optionA = upgradeButton("Fast Bake (cost 10) ", orginalCostA, 0.1);
+const optionB = upgradeButton("Faster Bake (cost 100) ", orginalCostB, 2.0);
+const optionC = upgradeButton("Fastest Bake (cost 1000) ", orginalCostC, 50.0);
 
 //logic for upgrade buttons
 function upgradeButton(text: string, cost: number, rate: number) {
@@ -78,10 +115,11 @@ function upgradeButton(text: string, cost: number, rate: number) {
       counter -= cost;
       speedRate += rate;
 
-      cost *= 1.15; //costs of each option increases by this increment. 
+      //costs of each option increases by this increment.
+      cost *= 1.15; 
       cost = parseFloat(cost.toFixed(2));
 
-      button2.innerText = `${text.split('(')[0]}(cost ${cost.toFixed(2)})`
+      button2.innerText = `${text.split("(")[0]}(cost ${cost.toFixed(2)})`;
 
       if (rate === 0.1) upgradeA++;
       else if (rate === 2.0) upgradeB++;
@@ -105,9 +143,9 @@ function enableButtons() {
 //updates counters
 function raiseCounter() {
   counter += 1;
-  counterDisplay.textContent = `${counter.toFixed(2)} cookies `;
+  counterDisplay.textContent = `${counter.toFixed(2)} `;
   speedRateDisplay.textContent = `Speed Rate: ${speedRate.toFixed(2)} cookies per second`;
-  buyDisplay.textContent = `Purchased - A: ${upgradeA}, B: ${upgradeB}, C: ${upgradeC}`;
+  buyDisplay.textContent = `Purchased - Fast Bake: ${upgradeA}, Faster Bake: ${upgradeB}, Fastest Bake: ${upgradeC}`;
 }
 
 function counterUpdate(timeLapse: number) {
@@ -117,7 +155,7 @@ function counterUpdate(timeLapse: number) {
 
   const delta = timeLapse - time;
   counter += (delta / 1000) * speedRate;
-  counterDisplay.textContent = `${counter.toFixed(2)} cookies `;
+  counterDisplay.textContent = `${counter.toFixed(2)} `;
   time = timeLapse;
   requestAnimationFrame(counterUpdate);
 }
@@ -136,7 +174,9 @@ setInterval(() => {
 }, 1000);
 
 //for elements to appear on the window
-app.append(counterDisplay);
+fryingPanResizeable.append(fryingPanPic);
+fryingPanResizeable.append(counterDisplay);
+app.append(fryingPanResizeable);
 app.append(header);
 app.append(button);
 app.append(optionA);
@@ -145,3 +185,4 @@ app.append(optionC);
 app.append(speedRateDisplay);
 app.append(buyDisplay);
 app.append(cookiePic);
+app.append(kitchenPic);
